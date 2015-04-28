3590758c3d5f4de6a8b48a5bc54815e5c9087fb3
MaJessica9 <jamma@ucdavis.edu>
2015-04-28 14:59:39 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,59 @@
+// Author: Sean Davis 
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+#include <fstream>
+#include "vector.h"
+using namespace std;
+
+void run(const Vector *cities)
+{
+  char airport1[80], airport2[80];
+  int index1, index2;
+  
+//  for(index1 = 0; index1 < cities->count; index1++)
+//    for(index2 = index1 + 1; index2  < cities->count; index2++)
+//      calcDistance(cities, index1, index2);
+
+  while (1)
+  {
+    cout<<"\nPlease enter two airport abbreviations (XXX XXX = done): ";
+    scanf("%s %s", airport1, airport2);
+    
+    if (strcmp(airport1, "XXX") == 0 && strcmp(airport2, "XXX") == 0)
+      break;
+    
+    index1 = cities->findAirport(airport1);
+    index2 = cities->findAirport(airport2);
+    
+    if (index1 >= 0 && index2 >= 0)
+      cities->calcDistance(index1, index2);
+  }  // infinite while loop
+}  // run())
+
+
+int main(int argc, char** argv) 
+{
+  Vector cities;
+  cities.initialize();
+  cities.readCities();
+  cities.readAirports();
+  cities.cleanCities();
+
+  srand(1);
+  int i;
+  for(i = 0; i < cities.count; i++){
+    printf("%s %s\n", cities.cityArray[i].airport, cities.cityArray[rand() % cities.count].airport);
+    printf("%s %s %lf %lf\n", cities.cityArray[i].name, cities.cityArray[i].airport, cities.cityArray[i].latitude, cities.cityArray[i].longitude);
+  }
+  run(&cities);
+  cities.deallocate();
+  return 0;
+} // main())
