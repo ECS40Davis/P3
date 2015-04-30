ae6a1b6677e496bf2bca68bbcdde5976dd463772
MaJessica9 <jamma@ucdavis.edu>
2015-04-30 00:31:25 -0700

    Update _city.cpp

--- _city.cpp
+++ _city.cpp
@@ -1,132 +1,150 @@
+
 #include "city.h"
 
-void City::initialize()
+City::City():name(NULL), state(NULL)
 {
-    name = new char[50];
-    state = new char[50];
-    airport = new char[50];
-    longitude = 0.0;
-    latitude = 0.0;
-    population = 0;
-    
-} // initialize()
+  airport[0] = '\0';  // sentinel value
+} // default constructor
 
-void City::readAirport(char *airports)
-{
 
-    if (airports)
-    {
-        char tempairport[100];
-        sscanf(airports, "%s  %f  %f  %[^,]%*c", tempairport,
-                       &latitude, &longitude, name);
-        std::string s (tempairport);
-        this->airport = strdup(s.substr(1, 3).c_str());
-        
-    }	
-} // readAirport()
+City::~City()
+{
+  if (name)
+    delete [] name;
 
-void City::readCities(char *n, char *st, float pop)
-{
-    this->name = n;
-    this->state = st;
-    this->population = pop;
-    
-} // readCities()
-                      
-bool City::isEqual(const City *city1, const City *city2)
-{
+  if (state)
+    delete [] state;
+}  // default destructor
 
-    if (strcmp(city2->name, city1->name) == 0)
-        return true;
-    return false;
-        
-} // isEqual()
-                      
-void City::copyLocation(const City *city2)
+City& City::operator=(const City &rhs)
 {
-    this->airport = city2->airport;
-    this->latitude = city2->latitude;
-    this->longitude = city2->longitude;    
-    
-} // copyLocation()
-                      
-void City::deallocate()
+  if (this == &rhs)
+    return *this;
+
+  longitude = rhs.longitude;
+  latitude = rhs.latitude;
+
+  if (name)
+    delete [] name;
+
+  if (rhs.name)
+  {
+    name = new char[strlen(rhs.name) + 1];
+    strcpy(name, rhs.name);
+  } // if (rhs.name)
+
+  if (state)
+    delete [] state;
+
+  if (rhs.state)
+  {
+    state = new char[strlen(rhs.state) + 1];
+    strcpy(state, rhs.state);
+  } // if (rhs.state)
+
+  strcpy(airport, rhs.airport);
+  population = rhs.population;
+  return *this;
+} // operator=
+
+void City::calcDistance(const City *city1)const
 {
-    
-    delete name;
-    delete state;
-    //fre(airport);
-        
-} // deallocate()
-                      
-void City::setAirport(const char *airportName)
-{    
-    airport = strdup(airportName);
-}//void City::setAirport(const char *airportName)
-                      
+  int distance, passengers;
+  
+  passengers = (double) city1->population * population / 2500000000;
+  distance = acos(
+    sin(city1->latitude * M_PI / 180) * sin(latitude * M_PI / 180) 
+    + cos(city1->latitude * M_PI / 180) * cos(latitude * M_PI / 180)
+    * cos((city1->longitude - longitude) * M_PI / 180)) * 3963;
+  
+  if (distance < 100)
+    passengers = 0;
+
+  cout << passengers << " passengers fly the " << distance << " miles from\n"
+      << city1->name << ", " << city1->state << " to " << name << ", " << state
+      << ".\n";
+}  // calcDistance())
+
+void City::copyLocation(const City *srcCity)
+{
+  strcpy(airport, srcCity->airport);
+  latitude = srcCity->latitude;
+  longitude = srcCity->longitude;
+}  // copyLocation()
+
+
 bool City::hasAirport()
 {
-    char c;
-    int i = 0;
-    
-    while (this->airport[i])
-    {
-        c = this->airport[i];
+  return this->airport[0] != '\0';
+}  // hasAirport()
 
-        if (!isupper(c))
-            return false;
-        i++;
-    }//while (this->airport[i])
-    
-    return true;
-    
-} // hasAirport()
-                      
-void City::calcDistance(const City *city1, const City *city2)
+
+bool City::isEqual(const City *city)const
 {
-    City test;
-    int R = 3963;
-    int passengers;
-    int dist = floor(acos(sin(city1->latitude * M_PI / 180)
-               * sin(city2->latitude * M_PI / 180)
-               + cos(city1->latitude * M_PI / 180)
-               * cos(city2->latitude * M_PI / 180)
-               * cos(city1->longitude * M_PI / 180
-               - city2->longitude * M_PI / 180)) * R);
+  if (city->name && name)
+    return strcmp(city->name, name) == 0;
+  
+  if (city->airport[0] && airport[0])
+    return strcmp(city->airport, airport) == 0;
+  
+  return false;
+}  // isName()
 
-    if (dist < 100)
-        passengers = 0;
-
-    else // farthur than 100 miles
-        passengers = test.calcPopulation(city1, city2);
-
-    cout<<passengers<<" passengers fly the "<<dist<<" miles from\n";
-} // calcDistance()
-
-int const City::calcPopulation(const City *city1, const City *city2)
+void City::readCity(fstream *fp)
 {
-    return floor((city1->population * city2->population) / 250000000) ;
-    
-}//double City::calcPopulation(City city2)
+  string line;
+  char *ptr;
+  
+  if (!getline(*fp, line) || !strstr(line.c_str(), ","))
+    return;
 
-// New Functions
+  char *line_copy = strdup(line.c_str());
+  ptr = strtok(line_copy, ",");
+  
+  if (ptr)
+  {  
+    name = new char[strlen(ptr) + 1];
+    strcpy(name, ptr);
+    ptr = strtok(NULL, ",");
+    state = new char[strlen(ptr) + 1];
+    strcpy(state, ptr);
+    population = atoi(strtok(NULL, ",\n"));
+  } // if something on line
 
-int City::cmpAirport(City *city1, City *city2)
+  delete [] line_copy;
+}  // readCity()
+
+void City::readAirport(char *line)
 {
-  return strcmp(city1->airport, city2->airport);
-}//int cmpAirport(City city1, City city2);
+  char *ptr;
+  
+  strtok(line, "] ");
+  strcpy(airport, &line[1]);
+  latitude = atof(strtok(NULL, " "));
+  longitude = atof(strtok(NULL, " "));
+  ptr = strtok(NULL, ",") + 1;
+  name = new char[strlen(ptr) + 1];
+  strcpy(name, ptr);
+}  // readAirport
 
-int City::cmpAbv(char *abv)
-{
-  return strcmp(abv, airport);
-}//City::cmpAbv(char *abv)
 
-char* City::getName(void)
+void City::setAirport(const char *airpor)
 {
-  return name;
-}//City::getName(void)
+  strcpy(airport, airpor);
+}  // setAirport()
 
-char* City::getState(void)
+void City::calcAirportTraffic(const City *city1)const
 {
-  return state;
-}//char* City::getState(void)
+  int distance, passengers;
+  
+  passengers = (double) city1->population * population / 2500000000;
+  distance = acos(
+    sin(city1->latitude * M_PI / 180) * sin(latitude * M_PI / 180) 
+    + cos(city1->latitude * M_PI / 180) * cos(latitude * M_PI / 180)
+    * cos((city1->longitude - longitude) * M_PI / 180)) * 3963;
+  
+  if (distance < 100)
+    passengers = 0;
+
+  cout << city1->name << ", " << city1->state << ": " << passengers << "\n";
+}  // calcAirportTraffic())
