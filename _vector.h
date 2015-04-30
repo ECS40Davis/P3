cda9343d6a68225c61323178dedbfd26b91e30a2
MaJessica9 <jamma@ucdavis.edu>
2015-04-30 00:31:59 -0700

    Update _vector.h

--- _vector.h
+++ _vector.h
@@ -1,54 +1,33 @@
 #ifndef VECTOR_H
-  #define VECTOR_H
-
+#define	VECTOR_H
+// Author Sean Davis
 #include <iostream>
 #include <fstream>
-#include <cstdio>
 #include <cstdlib>
-#include <cmath>
 #include <cstring>
+
 #include "city.h"
 using namespace std;
 
-
 class Vector
 {
-        City *cityArray;
-        int size;
-        int count;
+public:
+  City *cityArray;
+  int size;
+  int count;
 
-        // double array siz if necessary
-        void resize();
-        
-        // return city index in cityarray that has specified airport, else -1
-        int findAirport(const char *airportName, const City *cityArr)const;
-        
-        void cleanCities();
-    
-        // kills memory leaks or unrecoverable space
-        void deallocate();
+  void resize();
 
-      public:
-
-        // Functions in Main
-        // sets default values
-        void initialize();
-
-        // reads airportsLL.txt, calls City's readAirport()
-        void readAirports();
-    
-        // read line from citypopulations.csv + store data in cityarray
-        void readCities();
-
-    
-        // Functions in Run
-        // calls City's calcDistance()
-        void calcDistance(char *abv1, char *abv2)const;
-        
-        char *returnname(char *abv1);
-        
-        char *returnStateName(char *abv1);
-        
-}; //endclass
-                      
-#endif
+public:
+  Vector(); //default constructor
+  ~Vector(); //default destructor
+  void calcDistance(int index1, int index2)const;
+  void cleanCities();
+  int findAirport(const char *airport)const;
+  void readAirports();
+  void readCities();
+  int getChoice()const;
+  void calcAirportTraffic()const;
+  void cmpCity()const;
+}; // Vector
+#endif	// VECTOR_H 
