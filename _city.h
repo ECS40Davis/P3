a0be61ba646b90cdbef68008ddc2685676f4a478
MaJessica9 <jamma@ucdavis.edu>
2015-04-30 00:31:39 -0700

    Update _city.h

--- _city.h
+++ _city.h
@@ -1,69 +1,34 @@
 #ifndef CITY_H
-  #define CITY_H
-
-#include <cstdlib>
-#include <cstdio>
-#include <cmath>
-#include <cctype>
-#include <cstring>
+#define	CITY_H
+// Author: Sean Davis
 #include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
 using namespace std;
 
 class City
-{        
+{
+public:
+  double longitude;
+  double latitude;
+  char *name;
+  char *state;
+  char airport[4];
+  int population;
 
-        float longitude;
-        float latitude;
-        char *name;
-        char *state;
-        char *airport;
-        float population;
- 
-      public:
-   
-        // sets default values
-        void initialize();
-    
-        // parses line from airpotLL.txt
-        void readAirport(char *airs);
-    
-        // parses line from citypops.csv
-        void readCities(char *n, char *st, float pop);
-            
-        // true if cities have identical nams
-        bool isEqual(const City *city1, const City *city2);
-        
-        //set airpot of local City variable of findAirport()
-        void setAirport(const char *airName);
-    
-        // DO THIS ONE LATER
-        void copyLocation(const City *city2);
-    
-        // true if city has airpot
-        bool hasAirport();
-    
-        // calculate distance between airpots
-        void calcDistance(const City *city1, const City *city2);
-        
-        int const calcPopulation(const City *city1, const City *city2);
-    
-        // kills memory leaks or unrecoverable space
-        void deallocate();
-
-        // New functions
-        // Compare Airports
-        int cmpAirport(City *city1, City *city2);
-  
-        // Compare Abbreviations
-        int cmpAbv(char *abv);
-
-        // Get Name
-        char* getName(void);
- 
-        // Get State
-        char* getState(void);
-        
+public:
+  City(); //default constructor
+  ~City(); //default destructor
+  City& operator=(const City &rhs);
+  void calcDistance(const City *city1)const;
+  void copyLocation(const City *srcCity);
+  bool hasAirport();
+  bool isEqual(const City *city)const;
+  void readAirport(char *line);
+  void readCity(fstream *fp);
+  void setAirport(const char *airport);
+  void calcAirportTraffic(const City *city1)const;
 }; //endclass
-
-
-#endif
+#endif	// CITY_H 
