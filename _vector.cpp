2809632c5cea11c089529150a81a7291636b4797
MaJessica9 <jamma@ucdavis.edu>
2015-04-30 00:31:08 -0700

    Update _vector.cpp

--- _vector.cpp
+++ _vector.cpp
@@ -1,185 +1,218 @@
+// Author: Sean Davis
+
 #include "vector.h"
-#include "city.h"
-                      
-void Vector::initialize()
-{
-    count = 0;
-    size = 10;
-    cityArray = new City[10];
-        
-} // initialize()
-                      
-void Vector::resize()
-{
-  if (count == size)
-  {
-    City *newSpace = new City[2 * size];
-    
-    for (int i = 0; i < size; i++)
-    {
-        newSpace[i] = cityArray[i];
-        //cityArray[i].deallocate();
-        
-    } //for loop
-    //todo: delete entire cityArray
-    cityArray = newSpace;
-    size *= 2;
-      
-  } //if
 
-} // resize()
-                      
-void Vector::readAirports()
+Vector::Vector():size(10), count(0)
 {
-    //supposed to call isEqual() and copyLocation()
-    fstream file;
-    string line;
-    char airport[100];
-    float latitude;
-    float longitude;
-    char name[100];
-    file.open("airportLL.txt"); //, iostream
-    
-    while(std::getline(file, line))
-    {
-        char *cstring = strdup(line.c_str());
-        int fsf = sscanf(cstring, "%s  %f  %f  %[^\n]%*c", 
-                airport, &latitude, &longitude, name);
+  cityArray = new City[size];
+}  // default constructor
 
-        if (fsf == 4)
-        {
-            strtok(name, ",");
-            strtok(NULL, "/0");
-            City tempCity;
-            tempCity.initialize();
-            tempCity.readAirport(cstring);
-            
-            for (int i = 0; i < count; i++)
-            {
 
-                if (tempCity.isEqual(&cityArray[i], &tempCity))
-                {
-                    cityArray[i].copyLocation(&tempCity);
-                } //if
-            } //for
-        } //if
-    } //while
-} // readAirports()
-
-void Vector::readCities()
+Vector::~Vector()
 {
-    char *name;
-    char *state;
-    int population;
-    fstream file;
-    string line;
-    
-    file.open("citypopulations.csv");
-   
-    while(getline(file, line))
-    {
-        char *cstring = strdup(line.c_str());
-    
-        name = strtok(cstring, ",");
-        state = strtok(NULL, ",");
-        population = atoi(strtok(NULL, "\n"));
-        
-        City newCity;
-        newCity.initialize();
-        newCity.readCities(name, state, population);
-        
-    
-        this->cityArray[count] = newCity;
-        count++;
-        this->resize();
-    } //while
-                
-} // readCities()
-                      
-int Vector::findAirport(const char *airportName, const City *cityArr) const
+  delete [] cityArray;
+}  // deallocate())
+
+
+void Vector::calcDistance(int index1, int index2)const
 {
-    City city1;
-    city1.initialize();
-    city1.setAirport(airportName);
-    City city2;
-    
-    for (int i = 0; i < this->count; i++)
-    {
-        city2 = cityArr[i];
-     
-        if (city1.cmpAirport(&city1, &city2) == 0)
-        {
-            return i;
-        } //if
-        
-    } //for
-    
-    return -1;
-} //int Vector::findAirport                  
+  cityArray[index2].calcDistance(&cityArray[index1]);
+}  // calcDistance()
 
 void Vector::cleanCities()
 {
-    int airportCount = 0;   
-    
-    City *newSpace = new City[size];
-    
-    for (int i = 0; i < size; i++)
+  int i = 0;
+  
+  while (i < count)
+  {
+
+    if (!cityArray[i].hasAirport())
     {
-        
-        if (cityArray[i].hasAirport())
+      cityArray[i] = cityArray[--count];
+    }  // if city does not have an airport
+
+    else // city has an airport
+      i++;
+  }  // while more in array
+
+}  // cleanCities())
+
+
+int Vector::findAirport(const char* airpor)const
+{
+  City city;
+
+  city.setAirport(airpor);
+  
+  for (int i = 0; i < count; i++)
+    if (cityArray[i].isEqual(&city))
+      return i;
+  
+  cout << airpor << " is not a valid airport.\n";
+  return -1;
+}  // findAirport()
+
+void Vector::readAirports()
+{
+  string line;
+  City city;
+
+  fstream file;
+  file.open("airportLL.txt");
+
+  while (std::getline(file, line))
+  {
+
+    if (line[0] == '[')
+    {
+      char * temp = new char[line.size() + 1];
+      copy(line.begin(), line.end(), temp);
+      temp[line.size()] = '\0';
+      city.readAirport(temp);
+      
+      for (int i = 0; i < count; i++)
+      {
+
+        if (cityArray[i].isEqual(&city))
         {
-            newSpace[airportCount] = cityArray[i];
-            cityArray[i].deallocate();
-            airportCount++;
-            
-        } //if
-        
-    } //for
-//todo: delete entire cityArray
-    count = airportCount;
-    cityArray = newSpace;
-} //end cleanCity
+          cityArray[i].copyLocation(&city);
+          break;
+        }  // if found a matching name
 
-void Vector::calcDistance(char *abv1, char *abv2) const
-{
-    int index = findAirport(abv1, cityArray);
-    City city1 = cityArray[index];
-    
-    int index2 = findAirport(abv2, cityArray);
-    City city2 = cityArray[index2];
-    
-    city1.calcDistance(&city1, &city2);
-    
-    //return cityArray[findAirport(abv1)].calcDistance(cityArray[findAirport(abv2)])
-    
-} // calcDistance()
+      } // for all cities
 
-void Vector::deallocate()
+    }  // if an airport line
+
+  }  // while
+
+}  // readAirports()
+
+void Vector::readCities()
 {
+  fstream file;
+  file.open("citypopulations.csv");
+
+  while(!file.eof())
+  {
+    if (size == count)
+      resize();
+
+    cityArray[count++].readCity(&file);
+  } // while more in file
+  
+  count--;
+  file.close();
+}  // readCities()
+
+
+void Vector::resize()
+{
+  int i;
+  City *temp = new City[2 * size];
+
+  for (i = 0; i < size; i++)
+  {
+    temp[i] = cityArray[i];
+  } // for all cities
+  
+  size *= 2;
+  delete [] cityArray;
+  cityArray = temp;
+}  // resize()
+
+int Vector::getChoice()const
+{
+    char input[79];
+    int choice;
+    // Menu
+    cout << "\nFlight Simulator Menu\n";
+    cout << "0. Done.\n";
+    cout << "1. Determine distance and passengers between two airports.\n";
+    cout << "2. Determine all traffic from one airport.\n\n";
+    cout << "Your choice (0 - 2): ";
     
+    cin>>input;
+    cin.ignore(256, '\n');
+
+    if (isdigit(input[0]))
+    {
+        choice = atoi(input);
+
+        switch (choice)
+        {
+            case 0:
+                return 0;
+            case 1:
+                return 1;
+            case 2:
+                return 2;
+            default:
+                cout << "Your choice must be between 0 and 2.\
+                        Please try again.\n";
+                return -1;   
+        }//switch (input)
+
+    }//if (isdigit(input[0])
+
+    else // not a digit
+    {
+        cout << "Your choice must be between 0 and 2. Please try again.\n" ;
+        return -1;
+    }// else
+
+} // getChoice()
+
+void Vector::calcAirportTraffic()const
+{
+  char airport1[80];
+  int index;
+
+  
+  cout << "\nPlease enter an airport abbreviation (XXX): ";
+  cin>>airport1;
+
+  index = findAirport(airport1);
+
+  if (index >= 0)
+  {
+
     for (int i = 0; i < count; i++)
-        cityArray[i].deallocate();
-    
-    delete [] cityArray;
-        
-} // deallocate()
+    {
 
-char* Vector::returnname(char *abv1)
-{
-    //takes in abbreviation, searches thru array for abv, return name of abv
-    for (int i = 0; i < count; i++)
-        if (cityArray[i].cmpAbv(abv1) == 0)
-            return cityArray[i].getName();
-    return strdup("City Name Not Found");
-    
-} //returnname
+      if (i != index)
+        cityArray[index].calcAirportTraffic(&cityArray[i]);
+//        cityArray->calcAirportTraffic(index, i);
+    } // if not the same airport
 
-char* Vector::returnStateName(char *abv1)
+  } // if airport exists
+
+} // calcAirportTraffic()
+
+void Vector::cmpCity()const
 {
 
-    for (int i = 0; i < count; i++)
+  char airport1[80], airport2[80];
+  int index1, index2;
+  cout << "\nPlease enter two airport abbreviations (XXX XXX): ";
+  cin>>airport1>>airport2;
+    
+  index1 = findAirport(airport1);
+  index2 = findAirport(airport2);
+    
+  if (index1 >= 0 && index2 >= 0)
+    calcDistance(index1, index2);
+} // cmpCity()
 
-        if (cityArray[i].cmpAbv(abv1) == 0)
-            return cityArray[i].getState();
-    return strdup("State Name Not Found");
-} //returnStateName
+
+
+
+
+
+
+
+
+
+
+
+
+
